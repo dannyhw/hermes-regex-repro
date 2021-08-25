21f85ff47dfa0f450840f27e5096f361d236befe
Daniel Williams <danny.h.williams@hotmail.com>
2021-08-25 12:33:43 +0100

    refactor: more readable

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import {Text} from 'react-native';
 import React from 'react';
 
 const MESSAGE_REPEAT_AMOUNT = 1000;
-const LONG_MESSAGE =
-  `Labore ad cupidatat dolor dolor officia consectetur voluptate veniam aute nisi fugiat pariatur. Tempor culpa quis non duis cillum. Aliquip enim consectetur commodo occaecat commodo id deserunt exercitation magna dolor elit in duis culpa. Esse aliqua excepteur ipsum non non occaecat voluptate et consequat veniam aliquip qui ipsum.`.repeat(
-    MESSAGE_REPEAT_AMOUNT,
-  );
+
+const MESSAGE_TO_REPEAT = `Labore ad cupidatat dolor dolor officia consectetur voluptate veniam aute nisi fugiat pariatur. Tempor culpa quis non duis cillum. Aliquip enim consectetur commodo occaecat commodo id deserunt exercitation magna dolor elit in duis culpa. Esse aliqua excepteur ipsum non non occaecat voluptate et consequat veniam aliquip qui ipsum.`;
+const LONG_MESSAGE = MESSAGE_TO_REPEAT.repeat(MESSAGE_REPEAT_AMOUNT);
+
 export default () => {
   // @ts-ignore (hermes internal global variable not in global type)
   const isHermes = !!global.HermesInternal;
